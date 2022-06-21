c58976a66c0199c77dc5bb275baccf6d1e90b28b
Patricia Correia <pncorreia25@gmail.com>
2022-06-21 19:08:46 -0300

    requisito 3 e 4 reparos

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card">
+        <img
+          data-testid="planet-name"
+          src={ planetImage }
+          alt={ `Planets ${planetName}` }
+        />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
