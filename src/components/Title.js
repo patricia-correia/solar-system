ba574802de20003f4d7916f645ff81be0ae87a02
Patricia Correia <pncorreia25@gmail.com>
2022-06-21 17:43:55 -0300

    requisito 3 e 4 teste

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Title extends React.Component {
   render() {
     return (
-      <h2>{this.props.headline}</h2>
+      <h2>
+        {this.props.headline}
+      </h2>
     );
   }
 }
